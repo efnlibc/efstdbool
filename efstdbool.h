8bbb04367a275bf3d514f4b2d3503e1d8f30606e
Efgeen <efgeen1998@gmail.com>
2024-06-19 10:13:57 +0200

    Update efstdbool.h

--- efstdbool.h
+++ efstdbool.h
@@ -1,9 +1,14 @@
-#ifndef _EFSTDBOOL_H
-#define _EFSTDBOOL_H
+#ifndef EFSTDBOOL_H
+#define EFSTDBOOL_H
 
-#define bool unsigned char
+#ifndef __STDC_VERSION__
+#define _Bool unsigned char
+#endif
+
+#define bool _Bool
 #define true 0x01
 #define false 0x00
+#define __bool_true_false_are_defined 0x01
 
 #endif
 
