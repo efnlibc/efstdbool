c0d87320d6b46d61620ba12c86b4ae42cf510a90
Efgeen <efgeen1998@gmail.com>
2024-06-20 12:49:32 +0200

    Update efstdbool.h

--- efstdbool.h
+++ efstdbool.h
@@ -1,14 +1,34 @@
-#ifndef EFSTDBOOL_H
-#define EFSTDBOOL_H
+/* Copyright 2024 Efgeen */
+
+#ifndef _EFSTDBOOL_H
+#define _EFSTDBOOL_H
 
 #ifndef __STDC_VERSION__
 #define _Bool unsigned char
 #endif
 
-#define efbool _Bool
-#define eftrue 0x01
-#define effalse 0x00
-#define __efbool_eftrue_effalse_are_defined 0x01
+/*********************************/
+/* >>ISO/IEC 9899:1999 §7.16, ¶2 */
+/*********************************/
+
+/* >>bool */
+
+#define bool _Bool
+
+/*********************************/
+/* >>ISO/IEC 9899:1999 §7.16, ¶3 */
+/*********************************/
+
+/* >>true */
+
+#define true 0x01
+
+/* >>false */
+
+#define false 0x00
+
+/* >>__bool_true_false_are_defined */
+
+#define __bool_true_false_are_defined 0x01
 
 #endif
-
