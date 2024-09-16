710f7749022d65cf300ac5895119a5f6974a0529
Efgeen <efgeen1998@gmail.com>
2024-09-17 00:49:42 +0200

    .

---
+++ efstdbool.h
@@ -0,0 +1,13 @@
+#ifndef _EFSTDBOOL_H
+#define _EFSTDBOOL_H
+
+#ifndef __STDC_VERSION__
+#define bool unsigned char
+#define true 0x01
+#define false 0x00
+#define __bool_true_false_are_defined 0x01
+#else
+#include <stdbool.h>
+#endif
+
+#endif
